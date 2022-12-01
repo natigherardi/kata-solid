392edc9a35d1591be5c4930631dfeb5886270231
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2022-12-01 21:16:09 +0100

    added kata files

---
+++ src/telemetry-diagnostic-controls-test.ts
@@ -0,0 +1,5 @@
+describe("Telemetry System", () => {
+  describe("TelemetryDiagnosticControls", () => {
+    it("CheckTransmission should send a diagnostic message and receive a status message response", () => {});
+  });
+});
